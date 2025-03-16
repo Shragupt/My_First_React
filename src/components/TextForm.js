bf3d67545d50476e0fdc49d245c2e4b4ec3aa1e3
Shradha Gupta <shradhagupta2111@gmail.com>
2025-03-16 13:46:21 -0700

    Added more functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,13 +15,23 @@ export default function TextForm(props) {
   const [text, setText] = useState('Enter text here');
   
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox"rows="10">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox"rows="6">
         </textarea>
       </div>
       <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
+      </div>
+
+    <div className="conatiner my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
     </div>
+    </>
   );
 }
